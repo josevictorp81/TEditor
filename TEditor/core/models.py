067aabe08d2706e4e6169e1f644fe5899f9b3f76
josevictorp81 <josevictorp81@gmail.com>
2021-05-20 21:02:33 -0300

    model

---
+++ TEditor/core/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from ckeditor.fields import RichTextField
+from django.contrib.auth import get_user_model
+
+class Text(models.Model):
+    title = models.CharField('Titulo', max_length=100, blank=False, null=False)
+    content = RichTextField(blank=True, null=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    create = models.DateTimeField(auto_now_add=True)
+    create = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Texto'
+    
+    def __str__(self):
+        return self.title
