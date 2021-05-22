bef5f2d6fe57b2cbdb210c53958d55a12a79fb66
josevictorp81 <josevictorp81@gmail.com>
2021-05-21 22:59:34 -0300

    app acoounts

--- TEditor/TEditor/urls.py
+++ TEditor/TEditor/urls.py
@@ -1,8 +1,10 @@
+from django import urls
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
